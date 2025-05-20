19411e783e69d06b93ca79f7dfb84141ba0b61ab
fadhillaxl <fadhilla.php@gmail.com>
2025-05-21 01:12:16 +0700

    Update client.blade.php

--- resources/views/layouts/client.blade.php
+++ resources/views/layouts/client.blade.php
@@ -120,7 +120,7 @@
             <div class="container">
                 <a class="navbar-brand" href="/">
                     <img src="/images/logo2.png" alt="PLAYCROWD Logo">
-                    PLAYCROWD
+                    PLAYCROWDid
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                     <span class="navbar-toggler-icon"></span>
