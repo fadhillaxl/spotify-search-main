28c9303cd2bb1fad5564207a7f35cdc4ebbad125
fadhillaxl <fadhilla.php@gmail.com>
2025-05-21 01:16:19 +0700

    Update client.blade.php

--- resources/views/layouts/client.blade.php
+++ resources/views/layouts/client.blade.php
@@ -118,19 +118,38 @@
     <body>
         <nav class="navbar navbar-expand-lg navbar-dark">
             <div class="container">
-                <a class="navbar-brand" href="{{ route('dashboard') }}">
+                <a class="navbar-brand" href="/">
                     <img src="/images/logo2.png" alt="PLAYCROWD Logo">
                     PLAYCROWDid
                 </a>
-                a
+                <div class="nav">
+                <ul class="navbar-nav ms-auto">
+                        
+                        <li class="nav-item">
+                            <a href="{{ route('bands') }}" class="nav-link">Bands</a>
+                        </li>
+                        @if (Route::has('login'))
+                            @auth
+                                <li class="nav-item">
+                                    <a href="{{ url('/dashboard') }}" class="nav-link">Dashboard</a>
+                                </li>
+                            @else
+                                <li class="nav-item">
+                                    <a href="{{ route('login') }}" class="nav-link">Login</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{ route('register') }}" class="nav-link">Register</a>
+                                </li>
+                            @endauth
+                        @endif
+                    </ul>
+                </div>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item">
-                            <a href="http://asd">asdasd</a>
-                        </li>
+                        
                         <li class="nav-item">
                             <a href="{{ route('bands') }}" class="nav-link">Bands</a>
                         </li>
