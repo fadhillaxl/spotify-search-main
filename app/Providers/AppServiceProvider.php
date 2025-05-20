8e530cb8e1b69dce43b31b12c9dce0b71dfd338e
fadhillaxl <fadhilla.php@gmail.com>
2025-05-21 00:36:41 +0700

    provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,10 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
+    {   
+        if (request()->header('X-Forwarded-Proto') == 'https') {
+            URL::forceScheme('https');
+        }
         if (env('APP_ENV') === 'production') {
             URL::forceScheme('https');
         }
