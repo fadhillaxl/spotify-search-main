316d911f0da67cd3b44fc76b5eb0711ad18d2362
fadhillaxl <fadhilla.php@gmail.com>
2025-05-21 01:20:42 +0700

    Update client.blade.php

--- resources/views/layouts/client.blade.php
+++ resources/views/layouts/client.blade.php
@@ -123,52 +123,33 @@
                     PLAYCROWDid
                 </a>
                 <div>
-                    <ul class="navbar-nav ms-auto">
-                        
-                        <li class="nav-item">
-                            <a href="{{ route('bands') }}" class="nav-link">Bands</a>
-                        </li>
-                        @if (Route::has('login'))
-                            @auth
-                                <li class="nav-item">
-                                    <a href="{{ url('/dashboard') }}" class="nav-link">Dashboard</a>
-                                </li>
-                            @else
-                                <li class="nav-item">
-                                    <a href="{{ route('login') }}" class="nav-link">Login</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="{{ route('register') }}" class="nav-link">Register</a>
-                                </li>
-                            @endauth
-                        @endif
-                    </ul>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav ms-auto">
+                            <li class="nav-item">
+                                <a href="{{ route('bands') }}" class="nav-link">Bands</a>
+                            </li>
+                            @if (Route::has('login'))
+                                @auth
+                                    <li class="nav-item">
+                                        <a href="{{ url('/dashboard') }}" class="nav-link">Dashboard</a>
+                                    </li>
+                                @else
+                                    <li class="nav-item">
+                                        <a href="{{ route('login') }}" class="nav-link">Login</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="{{ route('register') }}" class="nav-link">Register</a>
+                                    </li>
+                                @endauth
+                            @endif
+                        </ul>
+                    </div>
                 </div>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <!-- <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ms-auto">
-                        
-                        <li class="nav-item">
-                            <a href="{{ route('bands') }}" class="nav-link">Bands</a>
-                        </li>
-                        @if (Route::has('login'))
-                            @auth
-                                <li class="nav-item">
-                                    <a href="{{ url('/dashboard') }}" class="nav-link">Dashboard</a>
-                                </li>
-                            @else
-                                <li class="nav-item">
-                                    <a href="{{ route('login') }}" class="nav-link">Login</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="{{ route('register') }}" class="nav-link">Register</a>
-                                </li>
-                            @endauth
-                        @endif
-                    </ul>
-                </div> -->
+                
+                
             </div>
         </nav>
 
